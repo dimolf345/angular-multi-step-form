c7b79e4d1e99b10c42593f0c877214ff410aaf78
Luca Di Molfetta <63596667+dimolf345@users.noreply.github.com>
2024-06-26 22:44:55 +0200

    feat: configure jest, unistall karma

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   template: `
-    <h1 class="text-3xl font-bold underline">Hello World</h1>
+    <h1 class="text-3xl font-bold underline">{{ title }}</h1>
     <button class="btn">Daisy btn</button>
   `,
   styleUrl: './app.component.scss',
