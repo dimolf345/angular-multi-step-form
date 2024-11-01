ef65d9dd87fe28e2a3a67fbf74e2e54d7a7038e2
dimolf345 <dimolfetta.ta@gmail.com>
2024-11-01 19:30:38 +0100

    feat: create success component

---
+++ src/app/steps/summary/success/success.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-success',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      success works!
+    </p>
+  `,
+  styles: ``
+})
+export class SuccessComponent {
+
+}
